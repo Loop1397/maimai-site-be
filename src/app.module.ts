8d53e6499401aae8d3de3bf6cedea9ae48093eec
Loop1397 <shwlsdn0906@gmail.com>
2024-06-11 23:46:52 +0900

    ✨ Feat: mongoDB 연결
    mongoose 패키지를 연결해 mongoDB를 사용하기위한 코드를 작성함

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SongsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    SongsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

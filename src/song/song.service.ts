121d80eb61473baa4c65cafc0ed38465a29a22c6
Loop1397 <shwlsdn0906@gmail.com>
2024-06-26 23:52:41 +0900

    ✨ Feat: song Get시 pattern 컬렉션 가져오는 기능 작성
    - getSongById에서 song을 불러올 때 patterns에 들어있는 objectId를 토대로 pattern의 정보를 가져옴
    -몇개 필요없는 필드는 뺌

--- src/song/song.service.ts
+++ src/song/song.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Song } from './schemas/song.schema';
 import { Model, Types } from 'mongoose';
@@ -10,28 +10,67 @@ export class SongService {
         private songModel: Model<Song>
     ) {}
 
+    /**
+     * TODO
+     * [x] : getSongById에서 populate가 제대로 적용되지 않는 문제 해결 - populate대신 aggregate로 일단 해결함
+     */
+
+    async getSongById(songId: Types.ObjectId) {
+        const song = await this.songModel.aggregate([
+            { $match: { _id: new Types.ObjectId(songId) } },
+            { 
+                $lookup: {
+                  from: 'pattern',
+                  localField: 'patterns',
+                  foreignField: '_id',
+                  as: 'patternDetails'
+                }
+            },
+            {
+                $project: {
+                    patternDetails: {
+                        _id:0,
+                        song:0,
+                        createdAt:0,
+                        updatedAt:0,
+                        __v:0
+                    }
+                }
+            }
+        ]).exec();
+
+        // 해당 songId에 맞는 패턴이 발견되지 않을 시
+        if(!song.length) {
+            throw new NotFoundException('해당 Id에 맞는 노래가 발견되지 않았습니다!');
+        }
+
+        // await foundedSong.populate({
+        //     // path: 외부 도큐먼트를 가져올(populate) 필드를 지정하는 옵션
+        //     path:'patterns',
+        // });
+
+        return song[0];
+    }
+
     async createSong(body: any) {
-        const { songName, artist, bpm, genre} = body;
+        const { title, artist, bpm, genre} = body;
+        console.log(body);
 
         // 동일한 노래 이름과 난이도를 가진 도큐먼트가 있는지 확인
         // https://progdev.tistory.com/29
-        const isSongExist = await this.songModel.findOne({songName});
+        const isSongExist = await this.songModel.findOne({title});
 
         if(isSongExist) {
             throw new UnauthorizedException('이미 DB에 존재하는 노래입니다!');
         }
 
         const song = await this.songModel.create({
-            songName,
+            title,
             artist,
             bpm,
             genre,
         });
 
         return song;
-    }
-
-    async addPattern(patternId: Types.ObjectId, song: Song) {
-        console.log(patternId, song);
     }
 }

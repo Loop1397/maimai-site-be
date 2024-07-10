64b58740098d89209ecf644ee99a01119c1b6e93
Loop1397 <shwlsdn0906@gmail.com>
2024-07-10 23:59:31 +0900

    ✨ Feat: signUpUser 작성
    friendCode를 입력받고 해당 코드를 토대로 회원가입하는 코드 작성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -6,4 +6,9 @@ export class UserController {
     constructor(
         private readonly userService: UserService
     ) {}
+
+    @Post()
+    async signUpUser(@Body() body: string) {
+        return await this.userService.signUpUser(body);
+    }
 }

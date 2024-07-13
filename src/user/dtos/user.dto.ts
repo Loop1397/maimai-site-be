0ca2df1861cc6c6d179b8a1c24f76010c37d777f
Loop1397 <shwlsdn0906@gmail.com>
2024-07-14 03:59:55 +0900

    🚜 Rename: createUserDto를 UserDto로 변경
    유저와 관련된 모든 요청이 동일한 형식의 데이터를 요구하기 때문에 validationPipe를 UserDto하나로 통일하기 위함

--- src/user/dtos/user.dto.ts
+++ src/user/dtos/user.dto.ts
@@ -1,6 +1,7 @@
 import { IsBoolean, Length, Matches } from "class-validator";
 
-export class createUserDto {
+// 유저와 관련된 모든 요청은 해당 dto에 따르도록 함
+export class UserDto {
     // 정규표현식(0부터 9사이의 숫자만)
     @Matches(RegExp('^[0-9]*$'))
     // 길이 13짜리 스트링으로 제한

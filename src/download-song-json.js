0347fc01ca259977dbc110267290e18542988278
Loop1397 <shwlsdn0906@gmail.com>
2024-07-17 23:24:36 +0900

    ✨ Feat: 중복되지 않은 노래 데이터 찾는 기능 작성
    노래 데이터 갱신 시 너무 많은 쿼리를 보내지 않기위해 이미 db에 저장된 데이터를 제외한 유니크한 데이터를 뽑아내는 함수 작성

--- src/download-song-json.js
+++ src/download-song-json.js
@@ -2,6 +2,32 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
+const readJsonFile = (filePath) => {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('#########################ERROR#########################', error);
+        return [];
+    }
+}
+
+const writeJsonFile = (filePath, data) => {
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+        console.log('JSON file updated successfully');
+    } catch (error) {
+        console.error('#########################ERROR#########################', error);
+    }
+};
+
+const findUniqueDatas = (existingData, newData) => {
+    const existingDataString = new Set(existingData.map(element => JSON.stringify(element)));
+    const uniqueDatas = newData.filter(element => !existingDataString.has(JSON.stringify(element)));
+    
+    console.log(uniqueDatas);
+}
+
 const downloadJsonFile = async (url, filePath) => {
     try {
         const response = await axios.get(url, { responseType: 'json' });
@@ -16,15 +42,18 @@ const downloadJsonFile = async (url, filePath) => {
         // catcode(장르)가 宴会場인 데이터 필터링
         const jsonData = response.data.filter(data => data.catcode !== `宴会場`);
 
+
         // 일본어 자판 ＆(아마 전각문자)를 영어 자판의 &로 바꿈
         jsonData.forEach(element => {
             element.catcode = element.catcode.replace(`＆`, `&`);
 
             // std, dx두 가지 패턴을 모두 가진 곡 찾기
-            if (Object.keys(element).length >= 16) console.log(element.title);
+            // if (Object.keys(element).length >= 16) console.log(element.title);
         });
 
-        fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+        findUniqueDatas(readJsonFile(filePath), jsonData);
+
+        writeJsonFile(filePath, jsonData);
         console.log(`download success!!!!!!`);
         /** 
          * TODO
@@ -40,5 +69,9 @@ const downloadJsonFile = async (url, filePath) => {
 
 const url = 'https://maimai.sega.jp/data/maimai_songs.json';
 const filePath = path.join(__dirname, '..', 'db/maimai_songs.json');
+
+console.time(`readFile`);
+readJsonFile(filePath);
+console.timeEnd(`readFile`);
 
 downloadJsonFile(url, filePath);
